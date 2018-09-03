ff0d60933dead29bf990e1a4fbff3577b7f2573a
alexkonkinmpt <p_a.a.konkin@mpt.ru>
2018-09-03 21:32:46 +0300

    Initial commit

---
+++ database/migrations/2018_09_03_093751_create_vehicles_table.php
@@ -0,0 +1,57 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVehiclesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vehicles', function (Blueprint $table) {
+            $table->increments('id')->unique();
+
+            $table->integer('user_id')->unsigned();
+            /*$table->foreign('user_id')
+            ->references('id')->on('users')
+            ->onDelete('CASCADE')->onUpdate('RESTRICT');*/
+
+            $table->string('vendor', 30);
+            $table->string('model', 30);
+            $table->integer('price');
+            $table->string('description', 255);
+
+            $table->integer('status_id')->unsigned();
+            $table->foreign('status_id')
+            ->references('id')->on('statuses')
+            ->onDelete('CASCADE')->onUpdate('RESTRICT');
+
+            $table->integer('type_id')->unsigned();
+            $table->foreign('type_id')
+            ->references('id')->on('types')
+            ->onDelete('CASCADE')->onUpdate('RESTRICT');
+
+            $table->integer('photo_id')->insigned();
+            $table->foreign('photo_id')
+            ->references('id')->on('photos')
+            ->onDelete('CASCADE')->onUpdate('RESTRICT');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vehicles');
+    }
+}
